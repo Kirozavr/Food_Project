5073838d6baa897c2cc20a6817d63fc62bdbe303
Kirill <Kirozavr@gmail.com>
2022-06-26 21:35:27 +0300

    3 commit 26.06.2022

--- js/script.js
+++ js/script.js
@@ -5,19 +5,36 @@ window.addEventListener("DOMContentLoaded", () => {
     function hideTabsContent() {
         // скрываем табы со страницы
         tabsContent.forEach((item) => {
-            item.style.display = 'none';
+            // добавляем классы, которые скрывают и показывают эелемент
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
         });
         // удаляем класс активности у каждого таба
         tabs.forEach((item) => {
             item.classList.remove('tabheader__item_active');
         });
     }
-    function showTabsContent(i = 0) {
-        // показываем первый таб на странице
-        tabsContent[i].style.display = 'block';
+    function showTabsContent(i = 0) {Ё
+        // показываем первый таб на странице. также назначем ему класс анимации
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
         // добавляем первому табу класс активности
         tabs[i].classList.add('tabheader__item_active');
     }
+    // назначаем обработчик событий на родителя
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+        // проверяем тыкнул ли пользователь в таб
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                // проверяем совпадает ли эелемент на который нажал пользователь с эелементом , который мы перебираем
+                if (target == item) {
+                    hideTabsContent();
+                    showTabsContent(i);
+                }
+            });
+        }
+    });
     hideTabsContent();
     showTabsContent();
 });
